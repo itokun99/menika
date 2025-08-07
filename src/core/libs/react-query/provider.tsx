225dc3c12d09f1b4de1327d11e191e4d00dae66b
Indrawan Lisanto <indrawanlisantopersonal@gmail.com>
2025-08-07 22:14:22 +0700

    fix: undefined import setupOnlineManager for react-query

--- src/core/libs/react-query/provider.tsx
+++ src/core/libs/react-query/provider.tsx
@@ -1,8 +1,8 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import React, { PropsWithChildren, useEffect } from 'react';
 import { appQueryClient } from './config';
-import { onAppStateChange, setupOnlineManager } from '.';
 import { AppState } from 'react-native';
+import { onAppStateChange, setupOnlineManager } from './utils';
 
 export const AppQueryProvider = ({ children }: PropsWithChildren) => {
   useEffect(() => {
