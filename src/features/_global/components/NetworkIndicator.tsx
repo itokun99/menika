5324f51210a830d208e7b3a1f39b876928ebeecf
Indrawan Lisanto <indrawanlisantopersonal@gmail.com>
2025-08-07 13:42:15 +0700

    refactor: move app to feature _root and setup online manager

---
+++ src/features/_global/components/NetworkIndicator.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react';
+import NetInfo from '@react-native-community/netinfo';
+import { Logger } from '@core/utils';
+import { showMessage } from 'react-native-flash-message';
+
+export const NetworkStatusIndicator = React.memo(() => {
+  const wasOffline = useRef(false);
+
+  useEffect(() => {
+    const unsubs = NetInfo.addEventListener(state => {
+      Logger.log(state);
+      if (!state.isConnected) {
+        showMessage({
+          message: 'Connection Lost!, you are currently offline',
+          type: 'warning',
+        });
+        wasOffline.current = true;
+        return;
+      }
+
+      if (state.isConnected && wasOffline.current) {
+        showMessage({
+          message: 'Back online!',
+          type: 'success',
+        });
+        wasOffline.current = false;
+      }
+    });
+
+    return () => {
+      unsubs();
+    };
+  }, []);
+
+  return null;
+});
